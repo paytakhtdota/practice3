73f9c0ef3de11e834bea803f87f91e795b1aefeb
Mo Ansari <paytakhtdota@gmail.com>
2023-12-24 21:15:16 -0800

    12-24-23 Still Practicing

--- script.js
+++ script.js
@@ -59,10 +59,11 @@ console.log("---------------------");
  att = document.createAttribute("calss");
  att.value = "red-text";
 document.getElementById("ref-link").setAttributeNode(att);
-document.getElementById("ref-link").setAttribute("class","red-text");
+document.getElementById("ref-link").setAttribute("class","red-text2");
  console.log(result);
  document.getElementById("ref-link").classList.add("new-class");
- document.getElementById("ref-link").classList.remove("new-class");
+ document.getElementById("ref-link").classList.remove("new-class");;
+ document.getElementById("ref-link").classList.remove("red-text2");
  console.log("---------------------");
  // ایجاد یک تگ جدید
 let newLi = document.createElement("li");
@@ -78,5 +79,45 @@ console.log(document.querySelector("ul").matches("a")); // چک می کند که
 console.log(document.querySelector("ul").matches("#to-do-list"));
 console.log(document.querySelector("img"));
 //از متود requestFullscreen() می توان برای فول اسکرین کردن المنت ها استفاده کرد
+// document.querySelector("img").requestFullscreen();
+// برای ایجاد شرتکات برای المنت ها از پروپرتی accessKey استفاده میکنم
+document.querySelector("#myform input").accessKey = "i";
+document.querySelector("#ref-link").accessKey = "l";
+// برای استفاده از کلید شرتکات ها باید از Aly+key استفاده کرد
+
+result = document.querySelector("#ref-link").attributes;
+console.log(result);
+
+// همه ی اتریبیوت ها نود هستند
+// در این جا سه پراپرتی هست که فقط برای نود های پراپرتی قابل استفاده است
+
+console.log(document.querySelector("#ref-link").attributes[1].name);
+console.log(document.querySelector("#ref-link").attributes[1].value);
+console.log(document.querySelector("#ref-link").attributes[1].ownerElement);
+// دریافت نود اتریبیوت با متود getNamedItem
+console.log(document.querySelector("#ref-link").attributes.getNamedItem("href"));
+
+document.querySelector("#ref-link").removeAttribute("class"); // با این متود می توان یک اتریبیوت را حذف کرد removeAttribute
+result = document.querySelector("#ref-link").attributes;// 
+console.error(result , `\n------------------------`);
 
 
+//مثالی زیبا از استفاده از متود append برای اضافه کردن فرزند به تگ های متفاوت
+document.addEventListener('DOMContentLoaded', function () { // این یک اوند لیسنر است کل کد داخل این اونت لیسنر قرار گرفته است برای این مطمئن شود این کد قبل از لود شدن کامل فایل اجتمل اجرا نخواهد شد
+    const parentElement = document.getElementById('parent');
+  
+    // اضافه کردن چند المان به انتهای المان والد
+    parentElement.append(
+      createChildElement("1 added dummy element"),
+      createChildElement("2 added dummy element"),
+      createChildElement("3 added dummy element")
+    );
+  
+    function createChildElement(content) {  //یک فانکشن برای ساخت سریع المنت و گذاشتن یک تکس داخل این المنت 
+      const childElement = document.createElement('div');
+      childElement.textContent = content;
+      return childElement;
+    }
+  });
+
+  console.log("______________________")
