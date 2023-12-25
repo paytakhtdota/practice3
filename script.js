19c17436f70445c7f968ec1676ebd73c90407b9b
Mo Ansari <paytakhtdota@gmail.com>
2023-12-24 23:47:50 -0800

    12-24-23 Still Practicing2

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ console.log(Array.from(tagNamesList)); // شما نمی توانید از فور
 Array.from(tagNamesList).forEach(element => {
     console.log(element);
 });
- 
+
 let htmlDoc = document.childNodes[1];
 console.log(htmlDoc);
 let myBody = htmlDoc.childNodes[2];
@@ -50,27 +50,27 @@ result = document.querySelector("#to-do-list").previousElementSibling;
 console.log(result);
 console.log("---------------------");
 // چطور به یک تگ اتریبیوت اضافه کنیم
- let att =document.createAttribute("target");
- att.value = "_self";
- document.getElementById("ref-link").setAttributeNode(att);
-
- // به یک تگ اتریبیوت اضافه کنیم
-
- att = document.createAttribute("calss");
- att.value = "red-text";
+let att = document.createAttribute("target");
+att.value = "_self";
 document.getElementById("ref-link").setAttributeNode(att);
-document.getElementById("ref-link").setAttribute("class","red-text2");
- console.log(result);
- document.getElementById("ref-link").classList.add("new-class");
- document.getElementById("ref-link").classList.remove("new-class");;
- document.getElementById("ref-link").classList.remove("red-text2");
- console.log("---------------------");
- // ایجاد یک تگ جدید
+
+// به یک تگ اتریبیوت اضافه کنیم
+
+att = document.createAttribute("class");
+att.value = "red-text";
+document.getElementById("ref-link").setAttributeNode(att);
+document.getElementById("ref-link").setAttribute("class", "red-text2");
+console.log(result);
+document.getElementById("ref-link").classList.add("new-class");
+document.getElementById("ref-link").classList.remove("new-class");;
+document.getElementById("ref-link").classList.remove("red-text2");
+console.log("---------------------");
+// ایجاد یک تگ جدید
 let newLi = document.createElement("li");
 let newSpan = document.createElement("span");
 let liText = document.createTextNode("dummy item five");
-newLi.setAttribute("class","to-do-item");
-newSpan.setAttribute("class","to-do-span");
+newLi.setAttribute("class", "to-do-item");
+newSpan.setAttribute("class", "to-do-span");
 newLi.appendChild(newSpan);
 newSpan.appendChild(liText);
 document.querySelector(".to-do-section ul").appendChild(newLi);
@@ -99,25 +99,47 @@ console.log(document.querySelector("#ref-link").attributes.getNamedItem("href"))
 
 document.querySelector("#ref-link").removeAttribute("class"); // با این متود می توان یک اتریبیوت را حذف کرد removeAttribute
 result = document.querySelector("#ref-link").attributes;// 
-console.error(result , `\n------------------------`);
+console.error(result, `\n------------------------`);
 
 
 //مثالی زیبا از استفاده از متود append برای اضافه کردن فرزند به تگ های متفاوت
 document.addEventListener('DOMContentLoaded', function () { // این یک اوند لیسنر است کل کد داخل این اونت لیسنر قرار گرفته است برای این مطمئن شود این کد قبل از لود شدن کامل فایل اجتمل اجرا نخواهد شد
     const parentElement = document.getElementById('parent');
-  
+
     // اضافه کردن چند المان به انتهای المان والد
     parentElement.append(
-      createChildElement("1 added dummy element"),
-      createChildElement("2 added dummy element"),
-      createChildElement("3 added dummy element")
+        createChildElement("1 added dummy element"),
+        createChildElement("2 added dummy element"),
+        createChildElement("3 added dummy element")
     );
-  
-    function createChildElement(content) {  //یک فانکشن برای ساخت سریع المنت و گذاشتن یک تکس داخل این المنت 
-      const childElement = document.createElement('div');
-      childElement.textContent = content;
-      return childElement;
-    }
-  });
 
-  console.log("______________________")
+    function createChildElement(content) {  //یک فانکشن برای ساخت سریع المنت و گذاشتن یک تکس داخل این المنت 
+        const childElement = document.createElement('div');
+        childElement.textContent = content;
+        return childElement;
+    }
+});
+
+console.log("______________________");
+
+document.querySelector("#ref-link").classList = "new-class red-text"
+
+result=document.querySelector("#ref-link").classList;
+console.log(result);
+
+document.querySelector("#ref-link").removeAttribute("class");
+console.log(document.querySelector("#ref-link").attributes);
+document.querySelector("#ref-link").className = "red-text";
+document.querySelector("#ref-link").removeAttribute("class");
+
+// ساده ترین روش و اسان ترین روش استفاده از متود setAttribute("","")
+
+document.querySelector("#ref-link").setAttribute("class","red-text");
+console.log(document.querySelector("#ref-link").attributes);
+
+// شما در اچتمل اتریبیوت شخصی خودتان که اصلا وجود ندارد را قرار بدهید برای گفتم مقدار اتریبیوت که میخواهد از متود زیر استفاده کنید
+document.querySelector("#ref-link").setAttribute("my-attribute","new-attribute");
+result = document.querySelector("#ref-link").getAttribute("my-attribute");
+console.log(result);
+
+ 
